9c5c56930eb8860c123ceef2758f5110ef2fcf55
Chris Gauthier <chrsgauthier5@gmail.com>
2018-10-02 06:31:59 -0400

    added some variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,10 @@
 var wins = 0;
+
+
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var currentwordText = document.getElementById("currentword-text");
+var guessesleftText = document.getElementById("guessesleft-text");
+var lettersguessedText = document.getElementById("lettersguessed-text");
+
 var 
